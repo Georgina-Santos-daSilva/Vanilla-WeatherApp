a1a88307a4e231d80779c1003cce1daa155836b7
Georgina-Santos-daSilva <georginadeartes@gmail.com>
2021-10-03 20:20:17 +0200

    Search engine added
    new forms and fuctions in JS

--- src/index.js
+++ src/index.js
@@ -45,8 +45,23 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "c4db4a0bb29288a24feb8d1fd9d4368e";
+function search(city) {
+  let apiKey = "c4db4a0bb29288a24feb8d1fd9d4368e";
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+search("Tokyo");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+//let city = "Berlin";
