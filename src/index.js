73be1fd1bb1845eb806401520a22498f58991220
Georgina-Santos-daSilva <georginadeartes@gmail.com>
2021-10-03 17:33:03 +0200

    Updated time fuctions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
 
@@ -7,6 +31,7 @@ function displayTemperature(response) {
   let cloudsElement = document.querySelector("#clouds");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
+  let dateElement = document.querySelector("#date");
 
   teperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -14,6 +39,7 @@ function displayTemperature(response) {
   cloudsElement.innerHTML = response.data.clouds.all;
   windElement.innerHTML = response.data.wind.speed;
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c4db4a0bb29288a24feb8d1fd9d4368e";
