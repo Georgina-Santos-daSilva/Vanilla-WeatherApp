449a2d4b521b11a354698adff189257a5c5843dc
Georgina-Santos-daSilva <georginadeartes@gmail.com>
2021-10-09 16:26:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -60,6 +61,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "c4db4a0bb29288a24feb8d1fd9d4368e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let teperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -81,6 +88,8 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -101,4 +110,3 @@ search("Tokyo");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-displayForecast();
